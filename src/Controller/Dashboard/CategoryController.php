ec2e52b02c589e8acbf5798213d52137a0c09431
Ahmed Helmy <helmyahmed248@gmail.com>
2019-07-20 00:26:36 +0200

    Database Commit

--- src/Controller/Dashboard/CategoryController.php
+++ src/Controller/Dashboard/CategoryController.php
@@ -1,10 +1,15 @@
 <?php
 // src/Controller/LuckyController.php
 namespace App\Controller\Dashboard;
-
+ 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Task;
 class CategoryController extends AbstractController
 {
     /**
@@ -22,8 +27,21 @@ class CategoryController extends AbstractController
     */
     public function create()
     {
-        
-        return $this->render('/dashboard/category/create.html.twig');
+        // creates a task and gives it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        return $this->render('/dashboard/category/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+     
     }
     
 }
--- src/Controller/Dashboard/ProductController.php
+++ src/Controller/Dashboard/ProductController.php
@@ -2,28 +2,48 @@
 // src/Controller/LuckyController.php
 namespace App\Controller\Dashboard;
 
+use App\Entity\Product;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProductController extends AbstractController
 {
-    /**
-     *
-     * @Route("/product", name="product_index")
-    */
-    public function index()
-    {
+     /**
+      *
+      * @Route("/product", name="product_index")
+     */
+     public function index()
+     {
     
-        return $this->render('/dashboard/product/index.html.twig');
-    }
+         return $this->render('/dashboard/product/index.html.twig');
+     }
     /**
-     *
-     * @Route("/product/create", name="product_create")
-    */
+     * @Route("/product", name="create_product")
+     */
     public function create()
     {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+        $category = new Category();
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setSalePrice(300);
+        $product->setPurchasePrice(200);
+        $product->setDescription('Ergonomic and stylish!');
+        $product->setStock(300);
+        $category->addProduct(1);
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
         
-        return $this->render('/dashboard/product/create.html.twig');
+        // return $this->render('/dashboard/product/create.html.twig');
     }
     
 }
